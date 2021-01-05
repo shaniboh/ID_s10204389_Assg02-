74c97b00a69413996e452cb4f7218abfe0aa540b
Shani Boh <s10204389@connect.np.edu.sg>
2021-01-05 12:44:27 +0800

    added channeltitle and video description

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
 
-
 $(function() {
     $("form").on("submit", function(e) {
-      e.preventDefault();
-      // prepare the request
-      var request = gapi.client.youtube.search.list({
-          part: "snippet",
-          type: "video",
-          q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-          maxResults: 10,
-          order: "viewCount",
-          publishedAfter: "2020-01-01T00:00:00Z"
-      }); 
-      // execute the request
-      request.execute(function(response) {
-        var results = response.result;
-        $("#results").html("");
-        $.each(results.items, function(index, item) {
-          $.get("item.html", function(data) {
-              $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+       e.preventDefault();
+       // prepare the request
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 10,
+            order: "viewCount",
+            //channel
+            publishedAfter: "2020-01-01T00:00:00Z"
+       }); 
+       // execute the request
+       request.execute(function(response) {
+          var results = response.result;
+          console.log(results);
+          $("#results").html("");
+          $.each(results.items, function(index, item) {
+            $.get("item.html", function(data) {
+                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId,"channeltitle":item.snippet.channelTitle, "description":item.snippet.description}]));
+                
+            });
+            
           });
-        });
-        resetVideoHeight();
-      });
+          resetVideoHeight();
+       });
     });
     
     $(window).on("resize", resetVideoHeight);
-
 });
 
 function resetVideoHeight() {
@@ -35,8 +37,8 @@ function resetVideoHeight() {
 }
 
 function init() {
-    gapi.client.setApiKey("AIzaSyAwtDvXArwabFqGIBHaVfgvaUQXlUfawxw");
+    gapi.client.setApiKey("AIzaSyCVOEUTAe2MBknKjo3zkgbYWDPTwmIoqkU");
     gapi.client.load("youtube", "v3", function() {
-        // youtube api is ready
+        // yt api is ready
     });
 }
